e22c45e9f7bd35de7de28c4e6e2fc69fe79e6925
Avveee <avveeerage@gmail.com>
2023-09-20 15:55:31 +0300

    seed categories

--- CoreMVC_Exam/Controllers/HomeController.cs
+++ CoreMVC_Exam/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace CoreMVC_Exam.Controllers
             _logger = logger;
         }
 
+        public IActionResult Categories()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
---
+++ CoreMVC_Exam/Migrations/20230920112653_seed_categories.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreMVC_Exam.Migrations
+{
+    /// <inheritdoc />
+    public partial class seed_categories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.Sql(@"
+        INSERT INTO Categories (id, name)
+        VALUES
+            ('RE0001', 'Refrigerators'),
+            ('WM0002', 'Washing Machines'),
+            ('DW0003', 'Dishwashers'),
+            ('MI0004', 'Microwaves'),
+            ('OV0005', 'Ovens'),
+            ('FR0006', 'Freezers'),
+            ('CM0007', 'Coffee Makers'),
+            ('TV0008', 'Televisions'),
+            ('VC0009', 'Vacuum Cleaners'),
+            ('BL0010', 'Blenders'),
+            ('IR0011', 'Irons'),
+            ('TO0012', 'Toasters'),
+            ('FP0013', 'Food Processors'),
+            ('HM0014', 'Hand Mixers'),
+            ('KE0015', 'Kettle');
+    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
