83678c1f3b1fadfe5a89cb7216af73dfbcc229cf
Avveee <avveeerage@gmail.com>
2023-09-22 15:02:33 +0300

    added categories crud

--- CoreMVC_Exam/Controllers/HomeController.cs
+++ CoreMVC_Exam/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using CoreMVC_Exam.Models;
+﻿using CoreMVC_Exam.Data;
+using CoreMVC_Exam.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CoreMVC_Exam.Controllers
@@ -7,15 +9,34 @@ namespace CoreMVC_Exam.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Categories()
         {
             return View();
+        }
+
+        public ActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        public ActionResult EditCategory(string id)
+        {
+            var cat = (from c in _context.Categories
+                       where c.id == id
+                       select c).FirstOrDefault();
+
+            if (cat == null)
+                return RedirectToAction("Index", "Au");
+
+            return View("EditCategory", cat);
         }
 
         public IActionResult Clients()
